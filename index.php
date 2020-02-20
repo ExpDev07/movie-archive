27e25603e31265bdce425d1b25187e3ae84c2763
ExpDev07 <marius.richardsen@hiof.no>
2020-02-20 11:13:57 +0100

    favicon

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
     <!-- Title -->
     <title>Movie Archive</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="/images/logo.png">
+
     <!-- Styling -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/styles.css">
